a1a4e36c7091c578bd2773aea2a32da0cbdc3582
Salvador <josesalvador0102@gmail.com>
2025-03-25 22:44:56 +0000

    asd

--- next.config.ts
+++ next.config.ts
@@ -18,8 +18,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; frame-src 'self' https://jscm.my.canva.site https://*.canva.site https://*.canva.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.canva.site https://*.canva.com; style-src 'self' 'unsafe-inline' https://*.canva.site https://*.canva.com; img-src 'self' data: https://*.canva.site https://*.canva.com;"
-          }
+            value: "frame-src 'self' https://jscm.my.canva.site/;",
+          },
         ],
       },
     ];
