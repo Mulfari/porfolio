cdfe68bb27bc68d4dd432fa188b55ff49cf7540b
Salvador <josesalvador0102@gmail.com>
2025-03-25 19:50:14 +0000

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
                 <div className="absolute inset-0 bg-gradient-to-br from-violet-600/20 via-transparent to-transparent z-10"></div>
                 
                 {/* Laptop en el centro */}
-                <div className="relative z-10 transform scale-[0.85] lg:scale-100">
+                <div className="relative z-10 transform scale-[0.85] lg:scale-100 animate-fade-in-up hover:scale-105 transition-all duration-300">
                   <Image
                     src="/icons/pc.png"
                     alt="Vista en laptop"
@@ -129,7 +129,7 @@ export default function Home() {
                 </div>
 
                 {/* Tablet a la izquierda */}
-                <div className="absolute left-4 lg:left-16 z-20 transform -translate-x-1/3 translate-y-4 scale-[0.4] lg:scale-[0.55] hover:scale-[0.6] transition-transform duration-300">
+                <div className="absolute left-4 lg:left-16 z-20 transform -translate-x-1/3 translate-y-4 scale-[0.4] lg:scale-[0.55] hover:scale-[0.6] transition-transform duration-300 animate-fade-in-up animation-delay-300">
                   <Image
                     src="/icons/tablet.png"
                     alt="Vista en tablet"
@@ -140,7 +140,7 @@ export default function Home() {
                 </div>
 
                 {/* Mobile a la derecha */}
-                <div className="absolute right-4 lg:right-16 z-20 transform translate-x-[40%] translate-y-8 scale-[0.35] lg:scale-[0.5] hover:scale-[0.6] transition-transform duration-300">
+                <div className="absolute right-4 lg:right-16 z-20 transform translate-x-[40%] translate-y-8 scale-[0.35] lg:scale-[0.5] hover:scale-[0.6] transition-transform duration-300 animate-fade-in-up animation-delay-500">
                   <Image
                     src="/icons/mobile.png"
                     alt="Vista en móvil"
